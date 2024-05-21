c92a6231a5e6a5b409202b12810c6868d9cc4565
rafao <rafael.chuchon@sptech.school>
2024-05-21 00:04:02 -0300

    runner added

--- runnerconfig.sh
+++ runnerconfig.sh
@@ -13,3 +13,14 @@ sudo usermod -a -G docker $(whoami)
 sudo systemctl start docker
 
 sudo docker run hello-world
+
+# Create a folder
+sudo mkdir actions-runner && cd actions-runner
+# Download the latest runner package
+sudo curl -o actions-runner-linux-x64-2.316.1.tar.gz -L https://github.com/actions/runner/releases/download/v2.316.1/actions-runner-linux-x64-2.316.1.tar.gz
+# Optional: Validate the hash
+sudo echo "d62de2400eeeacd195db91e2ff011bfb646cd5d85545e81d8f78c436183e09a8  actions-runner-linux-x64-2.316.1.tar.gz" | shasum -a 256 -c
+# Extract the installer
+sudo tar xzf ./actions-runner-linux-x64-2.316.1.tar.gz
+
+# nohup ./run.sh > output.log 2>&1 &
